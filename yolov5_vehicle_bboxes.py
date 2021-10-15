6ec295c979464cc2e5a7bd31f6df45b04b19ff90
Joe <king.joseph.m@gmail.com>
2021-10-15 08:46:42 -0400

    added try/except in case image blank to yolov5_vehicle_bboxes.py

--- yolov5_vehicle_bboxes.py
+++ yolov5_vehicle_bboxes.py
@@ -20,7 +20,10 @@ def detect_cars(path, model, min_confidence=0.5):
 
     assert((min_confidence >=0) and (min_confidence<=1)), "Object type confidence level bounded strictly between 0-1!"
 
-    results = model(path)  # applies NMS
+    try:
+        results = model(path)  # applies NMS
+    except ValueError:  # Image is blank
+        return []
 
     coordinates = results.xyxy[0].numpy()
 
@@ -32,43 +35,26 @@ def detect_cars(path, model, min_confidence=0.5):
     coordinates = coordinates[coordinates[:, 4] >= min_confidence]
     return coordinates[:, 0:5].tolist()
 
-def compile_image_directory(rootDir):
-
-    lst = []
-    for subdir, dirs, files in os.walk(rootDir):
-        for file in [i for i in files if 'jpg' in i or 'png' in i]:
-            lst.append('/'.join(os.path.join(subdir, file).split('/')[-4:]))  # does not count empty subdirectories
-    df = pd.DataFrame(lst, columns=["Orig Path"])
-    df['Make'] = df['Orig Path'].apply(lambda x: x.split('/')[0])
-    df['Model'] = df['Orig Path'].apply(lambda x: x.split('/')[1])
-    df['Year'] = df['Orig Path'].apply(lambda x: x.split('/')[2]).astype(int)
-    df['Orig Path'] = '/'.join(rootDir.split('/')[:-1]) + '/' + df['Orig Path']
-
-    return df[['Make', 'Model', 'Year', 'Orig Path']].sort_values(by=['Make', 'Model', 'Year']).reset_index(drop=True)
-
-
 if __name__ == '__main__':
 
     # Read in model
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
-    rootDir = '/Users/josephking/Documents/sponsored_projects/MERGEN/data/vehicle_classifier/scraped_images'
+    path = '/Users/josephking/Documents/sponsored_projects/MERGEN/data/vehicle_classifier/data_directories'
 
-    df = compile_image_directory(rootDir)
+    df = pd.read_csv(os.path.join(path, 'MakeModelDirectory.csv'))
 
     start = time()
 
     output = []
     count = 0
-    for i in df['Orig Path']:
+    for i in df['Source Path']:
         output.append(detect_cars(i, model, min_confidence=0.5))
         if (count % 1000 == 0) and (count > 0):
             print(f"Cumulative time after {count} images: {time() - start:.2f} sec\n")
         count += 1
     df['Bboxes'] = pd.Series(output)
 
-    outputDir = '/Users/josephking/Documents/sponsored_projects/MERGEN/data/vehicle_classifier/data_directories'
-
-    df.to_csv(os.path.join(outputDir, 'MakeModel_Bboxes.csv'), index=False)
+    df.to_csv(os.path.join(path, 'MakeModelDirectory_Bboxes.csv'), index=False)
 
     caffeine.off()
