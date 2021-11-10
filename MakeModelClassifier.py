6baa1c77b8db96b4127a4c243980bba828d77c39
Joe <king.joseph.m@gmail.com>
2021-11-10 15:45:35 -0500

    altered resnet argument to build ResNet50V2 model, instead of ResNet152V2

--- MakeModelClassifier.py
+++ MakeModelClassifier.py
@@ -164,7 +164,7 @@ class MakeModelClassifier(ClassifierCore):
                     raise ValueError("Please try training entire network")
 
         elif self.config['model'] == 'resnet':
-            pretrained_layer = resnet_v2.ResNet152V2(input_shape=(self.config['img_size'] + (3,)), include_top=False)
+            pretrained_layer = resnet_v2.ResNet50V2(input_shape=(self.config['img_size'] + (3,)), include_top=False)
 
             # Set last few layers as trainable - TODO
             if self.config['train_blocks'] < 0:
@@ -319,7 +319,7 @@ def parse_opt():
     parser.add_argument('--save-weights', type=str, choices=['true', 'false'], default='true', help='save model checkpoints and weights')
     parser.add_argument('--share-grayscale', type=float, default=0.5, help='share of training images to read in as greyscale')
     parser.add_argument('--confidence', type=float, default=0.50, help='object confidence level for YOLOv5 bounding box')
-    parser.add_argument('--model', type=str, default='mobilenet', choices=['mobilenet', 'resnet', 'xception', 'inception'], help='pretrained model type, options are `mobilenet` (MobileNetV2), `resnet` (ResNet152V2), `xception` (Xception), or `inception` (InceptionV3)')
+    parser.add_argument('--model', type=str, default='mobilenet', choices=['mobilenet', 'resnet', 'xception', 'inception'], help='pretrained model type, options are `mobilenet` (MobileNetV2), `resnet` (ResNet50V2), `xception` (Xception), or `inception` (InceptionV3)')
     parser.add_argument('--mobilenetv2-alpha', type=str, default='1.0', choices=['1.4', '1.3', '1.0', '0.75', '0.5', '0.35'], help='width multiplier in the MobileNetV2, options are 1.4, 1.3, 1.0, 0.75, 0.5, or 0.35')
     parser.add_argument('--learning-rate', type=float, default=0.001, help='Optimizer learning rate')
     parser.add_argument('--optimizer', type=str, default='adam', choices=['adam', 'adagrad', 'adamax', 'rmsprop'], help='Optimizer type, either `adam`, `adagrad`, `adamax` or `rmsprop`')
