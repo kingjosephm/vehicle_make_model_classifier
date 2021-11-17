3942271f1f7287d2c6a47bea555409372602b59c
Joe <king.joseph.m@gmail.com>
2021-11-17 16:14:31 -0500

    moved create_image_directory.py to `create_training_images` subdirectory

--- create_training_images/create_image_directory.py
+++ create_training_images/create_image_directory.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     ##### Merge vehicle type column #####
     #####################################
 
-    db = pd.read_csv('./create_training_images/make_model_database_mod.csv')
+    db = pd.read_csv('make_model_database_mod.csv')
     db = db[['Make', 'Model', 'Category']].drop_duplicates(subset=['Make', 'Model', 'Category']).reset_index(drop=True)
     db['Category'] = np.where(db.Category.isin(['Coupe', 'Sedan', 'Hatchback', 'Convertible', 'Wagon']), 'Car', db['Category'])
     db = db.drop_duplicates().reset_index(drop=True)
