d0a2fbf4e332b2dc8c1cec884fb453441ff035ce
Joe <king.joseph.m@gmail.com>
2021-11-24 08:31:08 -0500

    added functionality to yolov5_vehicle_bboxes.py to allow different sized yolo models to be selected

--- create_training_images/yolov5_vehicle_bboxes.py
+++ create_training_images/yolov5_vehicle_bboxes.py
@@ -3,14 +3,19 @@ import pandas as pd
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 from time import time
-import caffeine
 import os
 import numpy as np
 import argparse
 
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_colwidth', None)
-caffeine.on(display=True)
+
+try:  # if run locally, not on GPU server
+    import caffeine
+    caffeine.on(display=True)
+    skip = False
+except ModuleNotFoundError:
+    skip = True
 
 def detect_cars(path, model, min_confidence=0.5):
     """
@@ -32,7 +37,10 @@ def detect_cars(path, model, min_confidence=0.5):
     dims = np.array(results.imgs).shape
     dims = (dims[1], dims[2], dims[3])
 
-    coordinates = results.xyxy[0].numpy()
+    try:  # CPU implementation
+        coordinates = results.xyxy[0].numpy()
+    except TypeError:
+        coordinates = results.xyxy[0].cpu().numpy()
 
     if len(coordinates) == 0:  # no objects found
         return [], ()
@@ -63,7 +71,9 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--img-dir', type=str, help='path to CSV containing relative image paths and labels', required=True)  # stored locally at ./data
     parser.add_argument('--data', type=str, help='path to root directory where data located', required=True)
-    parser.add_argument('--min-confidence', type=float, default=0.5, help='minimum confidence level of YOLOv5 bounding box object type')
+    parser.add_argument('--min-confidence', type=float, default=0.0, help='minimum confidence level of YOLOv5 bounding box object type')
+    parser.add_argument('--yolo-model', type=str, default='small', choices=['small', 'medium', 'large', 'xl'], help='YOLOv5 model')
+    parser.add_argument('--output', type=str, required=True, help='output path including title of file')
     args = parser.parse_args()
     assert (args.min_confidence >= 0 and args.min_confidence < 1), 'min-codence param is bounded 0-1!'
     return args
@@ -72,7 +82,14 @@ def parse_opt():
 def main(opt):
 
     # Read in model, or download
-    model = torch.hub.load('yolov5', 'custom', path='./yolov5/yolov5s.pt', source='local')
+    if opt.yolo_model == 'small':
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    elif opt.yolo_model == 'medium':
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5m')
+    elif opt.yolo_model == 'large':
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5l')
+    else:
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5x')
 
     df = pd.read_csv(opt.img_dir)
 
@@ -82,7 +99,7 @@ def main(opt):
     count = 0
     for i in df['Source Path']:
 
-        coord, dims = detect_cars(os.path.join(opt.data, i), model, min_confidence=0.5)
+        coord, dims = detect_cars(os.path.join(opt.data, i), model, min_confidence=opt.min_confidence)
         output.append([coord, dims])
         if (count % 1000 == 0) and (count > 0):
             print(f"Cumulative time after {count} images: {time() - start:.2f} sec\n")
@@ -93,9 +110,10 @@ def main(opt):
 
     df = df.sort_values(by=['Make', 'Model'])
 
-    df.to_csv(os.path.join(opt.data, 'Bboxes.csv'), index=False)
+    df.to_csv(opt.output, index=False)
 
-    caffeine.off()
+    if skip:
+        caffeine.off()
 
 
 if __name__ == '__main__':
